764900a83835955856f89d2cf1db0c89d9f8ba73
pseex <evgeniyshakhov@gmail.com>
2021-05-01 09:17:24 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ecmascript.local</title>
+</head>
+<body>
+
+
+<script src="main.js?<?= rand(0, PHP_INT_MAX); ?>"></script>
+
+</body>
+</html>
